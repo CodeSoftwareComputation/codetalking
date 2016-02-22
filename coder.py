9118d299d2f5c77c8e09fa12eb2e77aa89d4a5dc
aberman <arthurberman94@gmail.com>
2016-02-22 14:12:02 -0500

    Add coder shell

---
+++ coder.py
@@ -0,0 +1,17 @@
+morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+         "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+
+class Coder:
+    def __init__(self, normal_form, encoded_form): # Construct a coder
+        pass
+
+    def encode(self, string): # encode a message
+        pass
+
+    def decode(self, string): # decode a message
+        pass
+
+
+class MorseCoder(Coder):
+    pass
